29907e0b3d58f91ca98c83743170e7e124a46cbf
Kelvin Penus <kp@brandscreen.com>
2016-04-06 15:43:00 +1000

    Add campaing list item

---
+++ src/js/actions/campaignListActionCreators.js
@@ -0,0 +1,15 @@
+var BSAPIAppDispatcher = require('../dispatcher/BSAPIAppDispatcher');
+var BSAPIConstants = require('../constants/BSAPIConstants');
+
+var ActionTypes = BSAPIConstants.ActionTypes;
+
+module.exports = {
+
+    receive: function(response) {
+        BSAPIAppDispatcher.dispatch({
+            type: ActionTypes.RECEIVE_CAMPAIGN_LIST,
+            response: response
+        });
+    }
+
+};
--- src/js/components/campaignlist.react.js
+++ src/js/components/campaignlist.react.js
@@ -1,69 +1,66 @@
 var CampaignListStore = require('../stores/CampaignListStore');
 var LoginActionCreators = require('../actions/LoginActionCreators');
+var CampaignListItem = require('../components/CampaignListItem.react');
 var React = require('react');
 
 function getStateFromStores() {
-  return {
-    campaigns: CampaignListStore.get()
-  };
+    return {
+        campaigns: CampaignListStore.get()
+    };
 }
 
 function getCampaignItem(campaign) {
-  return (
-    <tr>
-      <td>
-        <a href="foobar">Hello world</a>
-      </td>
-    </tr>
-  );
+    return (
+        <CampaignListItem key={campaign.CampaignUuid} campaign={campaign} />
+    );
 }
 
 var CampaignList = React.createClass({
 
-  getInitialState: function() {
-    return getStateFromStores();
-  },
+    getInitialState: function() {
+        return getStateFromStores();
+    },
 
-  componentDidMount: function() {
-    CampaignListStore.addChangeListener(this._onChange);
-  },
+    componentDidMount: function() {
+        CampaignListStore.addChangeListener(this._onChange);
+    },
 
-  componentWillUnmount: function() {
-    CampaignListStore.removeChangeListener(this._onChange);
-  },
+    componentWillUnmount: function() {
+        CampaignListStore.removeChangeListener(this._onChange);
+    },
 
-  render: function() {
-    var emptyElement;
-    var tableElement;
-    if (this.state.campaigns.length === 0) {
-      emptyElement = (<p>No Campaigns</p>);
-    } else {
-      var campaignItems = this.state.campaigns.map(getCampaignItem);
-      tableElement = (
-        <div>
-          <table className="table table-striped">
-            <tbody>
-              {campaignItems}
-            </tbody>
-          </table>
-          // TODO: pagination
-        </div>
-      );
+    render: function() {
+        var emptyElement;
+        var tableElement;
+        if (this.state.campaigns.length === 0) {
+            emptyElement = (<p>No Campaigns</p>);
+        } else {
+            var campaignItems = this.state.campaigns.map(getCampaignItem);
+            tableElement = (
+                <div>
+                  <table className="table table-striped">
+                    <tbody>
+                    {campaignItems}
+                    </tbody>
+                  </table>
+                  // TODO: pagination
+                </div>
+            );
+        }
+        return (
+            <div>
+                {emptyElement}
+              <div className="pull-right">
+                <input className="form-control" type="text" placeholder="Search" />
+              </div>
+                {tableElement}
+            </div>
+        );
+    },
+
+    _onChange: function(event, value) {
+        this.setState(getStateFromStores());
     }
-    return (
-      <div>
-        {emptyElement}
-        <div className="pull-right">
-          <input className="form-control" type="text" placeholder="Search" />
-        </div>
-        {tableElement}
-      </div>
-    );
-  },
-
-  _onChange: function(event, value) {
-    this.setState(getStateFromStores());
-  }
 });
 
 module.exports = CampaignList;
