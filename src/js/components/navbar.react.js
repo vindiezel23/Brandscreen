f5d7400c6c8b06c9ab2f219760cc746580d6fc6b
Kelvin Penus <kp@brandscreen.com>
2016-05-02 11:08:27 +1000

    Add react router package

--- src/js/components/navbar.react.js
+++ src/js/components/navbar.react.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var Link = require('react-router').Link;
 
 var Navbar = React.createClass({
 
@@ -7,19 +8,19 @@ var Navbar = React.createClass({
             <nav className="navbar navbar-default navbar-static-top">
                 <div className="container">
                     <div className="navbar-header">
-                        <a className="navbar-brand" href="#">Brandscreen API React Demo</a>
+                        <Link className="navbar-brand" to="/">
+                            Brandscreen API React Demo
+                        </Link>
                     </div>
                     <div id="navbar" className="navbar-collapse collapse">
                         <ul className="nav navbar-nav">
                             {/* TODO: dynamic active class */}
                             {/* TODO: react router links */}
-                            <li className="active"><a href="#">Home</a></li>
-                            <li><a href="#">Foo</a></li>
-                            <li><a href="#">Bar</a></li>
+                            <li className="active"><Link to="/">Home</Link></li>
                         </ul>
                         <ul className="nav navbar-nav navbar-right">
                             {/* TODO: dynamic logged in status */}
-                            <li><a href="#">Login</a></li>
+                            <li><Link to="/login">Login</Link></li>
                         </ul>
                     </div>
                 </div>
