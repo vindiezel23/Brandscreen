7dc048ac3f00057d0fd86679d3a6c74a6063526d
Kelvin Penus <kp@brandscreen.com>
2016-05-02 17:35:56 +1000

    Bootstrap

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 var BSAPIApp = require('./components/BSAPIApp.react');
 window.$ = window.jQuery = require('jquery');
-var bootstrap = require('bootstrap');
-console.log(bootstrap);
+require('bootstrap');
 var React = require('react');
 var ReactDOM = require('react-dom');
 window.React = React; // export for http://fb.me/react-devtools
