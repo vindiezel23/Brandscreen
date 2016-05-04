e5e5d0ee6465e46fbb1c177b4a2d1b54ce8ffcc2
Kelvin Penus <kp@brandscreen.com>
2016-05-04 13:20:53 +1000

    Dynamic login link class

--- src/js/components/navbar.react.js
+++ src/js/components/navbar.react.js
@@ -1,9 +1,30 @@
 var React = require('react');
 var Link = require('react-router').Link;
+var LoginStore = require('../stores/LoginStore');
 
 var Navbar = React.createClass({
 
+    getInitialState: function() {
+        return {username: ''};
+    },
+
+    componentDidMount: function() {
+        LoginStore.addLoginListener(this._onLogin);
+        LoginStore.addExpiredListener(this._onLogin);
+    },
+
+    componentWillUnmount: function() {
+        LoginStore.removeLoginListener(this._onLogin);
+        LoginStore.removeExpiredListener(this._onLogin);
+    },
+
     render: function() {
+        var loginText = 'Login';
+        var loginLinkClass = 'active';
+        if (this.state.username != '') {
+            loginText = 'Logged in as ' + this.state.username;
+            loginLinkClass = '';
+        }
         return (
             <nav className="navbar navbar-default navbar-static-top">
                 <div className="container">
@@ -19,13 +40,22 @@ var Navbar = React.createClass({
                             <li className="active"><Link to="/">Home</Link></li>
                         </ul>
                         <ul className="nav navbar-nav navbar-right">
-                            {/* TODO: dynamic logged in status */}
-                            <li><Link to="/login">Login</Link></li>
+                            <li className={loginLinkClass}>
+                                <Link to='/login'>{loginText}</Link>
+                            </li>
                         </ul>
                     </div>
                 </div>
             </nav>
         );
+    },
+
+    _onLogin: function() {
+        if (LoginStore.getAccessToken() === '') {
+            this.setState({username: ''});
+        } else {
+            this.setState({username: LoginStore.getUsername()});
+        }
     }
 
 });
