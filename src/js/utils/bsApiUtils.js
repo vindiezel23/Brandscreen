df72c0691ccd9e2c4827ca0071c6b56d92f7611a
Kelvin Penus <kp@brandscreen.com>
2016-05-18 10:19:47 +1000

    Refactor action stores

--- src/js/utils/bsApiUtils.js
+++ src/js/utils/bsApiUtils.js
@@ -1,9 +1,32 @@
 var $ = require('jquery');
 var CampaignListActionCreators = require('../actions/CampaignListActionCreators');
-var CampaignActionCreators = require('../actions/CampaignActionCreators');
+var ReceiveActionCreators = require('../actions/ReceiveActionCreators');
 var LoginActionCreators = require('../actions/LoginActionCreators');
 var LoginStore = require('../stores/LoginStore');
 var BSAPIConstants = require('../constants/BSAPIConstants');
+
+var ActionTypes = BSAPIConstants.ActionTypes;
+
+// Parameters
+// - path (i.e. GET url/api/<path>)
+// - actionType
+function getModelFunc(options) {
+    return function(id) {
+        var url = BSAPIConstants.url + 'api/' + options.path + '/' + id + '?UserId=';
+        $.ajax({
+            url: url,
+            headers: {'Authorization': 'Bearer ' + LoginStore.getAccessToken()}
+        }).done(function(response) {
+            ReceiveActionCreators.receive(response, options.actionType);
+        }).fail(function(error) {
+            if (error.status === 401) {
+                LoginActionCreators.expired();
+            } else {
+                console.log(error);
+            }
+        });
+    };
+}
 
 module.exports = {
 
@@ -20,16 +43,14 @@ module.exports = {
         });
     },
 
-    getCampaign: function(id) {
-        var url = BSAPIConstants.url + 'api/campaigns/' + id + '?UserId=';
-        $.ajax({
-            url: url,
-            headers: {'Authorization': 'Bearer ' + LoginStore.getAccessToken()}
-        }).done(function(response) {
-            CampaignActionCreators.receive(response);
-        }).fail(function(error) {
-            LoginActionCreators.expired();
-        });
-    }
+    getCampaign: getModelFunc({
+        path: 'campaigns', actionType: ActionTypes.RECEIVE_CAMPAIGN
+    }),
+    getBrand: getModelFunc({
+        path: 'brands', actionType: ActionTypes.RECEIVE_BRAND
+    }),
+    getAccount: getModelFunc({
+        path: 'accounts', actionType: ActionTypes.RECEIVE_ACCOUNT
+    })
 
 };
