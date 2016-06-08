8441f1b3a3deb0d0a91ca536abceb21c63efc0c9
Kelvin Penus <kp@brandscreen.com>
2016-06-08 10:14:21 +1000

    Change routing of lists

--- src/js/app.js
+++ src/js/app.js
@@ -7,20 +7,23 @@ window.React = React; // export for http://fb.me/react-devtools
 var ReactRouter = require('react-router');
 var LoginForm = require('./components/LoginForm.react');
 var Campaigns = require('./components/Campaigns.react');
-var CampaignList = require('./components/CampaignList.react');
+var CampaignList = require('./components/lists.react').Campaign;
 var Campaign = require('./components/Campaign.react');
 var Strategy = require('./components/Strategy.react');
+var models = require('./models/models');
 
 ReactDOM.render((
     <ReactRouter.Router history={ReactRouter.hashHistory}>
         <ReactRouter.Route path="/" component={BSAPIApp}>
             <ReactRouter.IndexRoute component={Campaigns} />
             <ReactRouter.Route path="login" component={LoginForm} />
-            <ReactRouter.Route path="campaign" component={Campaigns}>
+            <ReactRouter.Route path={models.Campaign.route} component={Campaigns}>
                 <ReactRouter.IndexRoute component={CampaignList} />
-                <ReactRouter.Route path=":CampaignUuid">
+                <ReactRouter.Route path={`:${models.Campaign.modelId}`}>
                     <ReactRouter.IndexRoute component={Campaign} />
-                    <ReactRouter.Route path="strategy/:StrategyUuid" component={Strategy} />
+                    <ReactRouter.Route
+                        path={`${models.Strategy.route}/:${models.Strategy.modelId}`}
+                        component={Strategy} />
                 </ReactRouter.Route>
             </ReactRouter.Route>
         </ReactRouter.Route>
